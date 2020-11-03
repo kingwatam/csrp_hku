1f2b849f7260ef0296cde4d3bde21b1e598f4770
King <king.wa.tam@gmail.com>
2020-11-03 11:20:32 +0800

    Back up filter for Meltwater suicide news

---
+++ early_warning/meltwater_search_terms.R
@@ -0,0 +1,29 @@
+((撒瑪利亞防止自殺會 or 醫院管理局精神健康專線 or 18288 or【珍惜生命】) 
+  or 
+  ((
+    (尋死 or 自尋短見 or 檢獲遺書 or 撿獲遺書 or 因病厭世 or 膠袋笠頭 or 氦氣 or 企跳) or
+    ((自殺 or 輕生 or 自殘 or 同歸於盡 or 殉情 or 死念) and 
+      (上吊 or 企圖 or 燒炭 or 自焚 or 自溺 or 自刎 
+        or 割頸 or 窒息 or 自縊 or 吊頸 or 上吊 
+        or 跳樓 or 跳軌 or 跳橋 or 跳崖 or 跳海
+        or 墜樓 or 墜軌 or 墜橋 or 墜崖 or 墜海
+        or 墮樓 or 墮軌 or 墮橋 or 墮崖 or 墮海 
+        or 墜斃 or 墮斃
+        or 服毒 or 服藥 or 割腕 or 𠝹手 
+        or 昏迷 or 危殆 or 命危 or 死亡 or 身亡 or 不治 or 喪命 or 斃命 or 送命 or 斃 or 企圖 or 氣絕
+        or 送院 or 報警 or 消防 or 警方 or 水警 or 報案 or 接報 or 救援 or 有可疑 or 無可疑)) 
+  ) 
+    not (國際 or 國際頭條 or 國際突發 or 國際新聞 or 氣候變化 or 當地 or 新華社 or 內地傳媒 or 廣州日報
+           or 台灣 or 新北市 or 南韓 or 韓國 or 首爾 or 北韓 or 日本 or 阿富汗 or 黎巴嫩 
+           or 內蒙 or 比利時 or 伊朗 or 法國 or 阿富汗 or 英超 or 俄羅斯 or 荷蘭 or 花蓮 
+           or 英國 or 美國 or 特朗普 or 河南 or 台北 or 歐洲 or 倫敦 or 墨西哥 
+           or 台中 or 泰國 or 國際專題 or 澳洲 or 珠海 or 敘利亞 or 波士頓 or 聯合國 or 巴黎 or 桑吉輪 
+           or 南非 or 普京 or 瑞士 or 耶城 or 以色列 or 孟加拉 or 貴州 or 大阪 or 東京 
+           or 新西蘭 or 紐西蘭 or 武漢 or 公安 or 浙江 or 意大利 or 台板 or 德國 or 全國 or 美國 or 紐約 or 洛杉磯 
+           or 澳大利亞 or 兩岸頭條 or 湖南 or 以巴 or 台男 or 台女 or 東莞 or 紐時 or 波音 or 波蘭 
+           or 河北 or 山西 or 遼寧 or 吉林 or 黑龍江 or 江蘇 or 浙江 or 安徽 or 福建 or 江西 or 山東 or 河南 
+           or 湖北 or 湖南 or 海南 or 四川 or 貴州 or 雲南 or 陝西 or 甘肅 or 內蒙古 or 廣西 or 西藏 
+           or 寧夏 or 新疆 or 北京 or 天津 or 重慶 or 加拿大 or 新加坡)
+    not (焦點 or 無綫 or 女星 or 男星 or 娛樂 or 曼聯 or 韓劇 or 日劇 or 奧斯卡 or 影后 or 影帝 or 皇室)) 
+)
+not (回顧 or 當地傳媒 or 去年 or 提堂 or 被告 or 續審 or 被審 or 被控 or 上訴 or 陪審團)
