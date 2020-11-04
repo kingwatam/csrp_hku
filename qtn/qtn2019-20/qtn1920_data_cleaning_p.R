abb3f87cdb6fb52b2db347ab861dbb290d5504bf
King <king.wa.tam@gmail.com>
2020-11-04 14:27:38 +0800

    Change part 8 to parts 9 & 10

--- qtn/qtn2019-20/qtn1920_data_cleaning_p.R
+++ qtn/qtn2019-20/qtn1920_data_cleaning_p.R
@@ -11,9 +11,10 @@ source("helper_functions.R")
 library(dplyr)
 
 setwd(sprintf("~%s/qtn/qtn2019-20/primary", setpath))
-# import pre & post-test data from sav files ----
-dfpre <- haven::read_sav("survey_450000_SPSS.sav")
-dfpost <- haven::read_sav("survey_450004_SPSS.sav")
+
+# P4 (level 1) ----
+dfpre <- haven::read_sav("P4-pre-survey_450000_SPSS.sav")
+dfpost <- haven::read_sav("P4-post-survey_450004_SPSS.sav")
 
 dfpre$T1 <- 0
 dfpost$T1 <- 1
@@ -51,10 +52,10 @@ df %>% select(starts_with("C01")) %>% colnames(.) -> P4
 df %>% select(starts_with("D01")) %>% colnames(.) -> P5
 df %>% select(starts_with("E01")) %>% colnames(.) -> P6
 df %>% select(starts_with("F01")) %>% colnames(.) -> P7
-df %>% select(starts_with("G01")) %>% colnames(.) -> P8a
-df %>% select(starts_with("G02")) %>% colnames(.) -> P8b
-df %>% select(starts_with("G0") & ends_with("SQ001")) %>% colnames(.) -> P8c
-df %>% select(starts_with("G05")) %>% colnames(.) -> P8d
+df %>% select(starts_with("G01")) %>% colnames(.) -> P9a
+df %>% select(starts_with("G02")) %>% colnames(.) -> P9b
+df %>% select(starts_with("G0") & ends_with("SQ001")) %>% colnames(.) -> P10a_b
+df %>% select(starts_with("G05")) %>% colnames(.) -> P10c
 
 reverse_P1a <- P1a[c(1,3,4,5,6,7,9,12,13,14,15)]
 df[reverse_P1a] <- (df[reverse_P1a]-1)*-1 # reverse (1:0) to (0:1)
@@ -76,131 +77,150 @@ df[reverse_P6] <- (df[reverse_P6]-5)*-1 # reverse (1:4) to (4:1)
 reverse_P7 <- P7[c(3, 6)]
 df[reverse_P7] <- (df[reverse_P7]-8)*-1 # reverse (1:7) to (7:1)
 
+reverse_P10c <- P10c[2]
+df[reverse_P10c] <- (df[reverse_P10c]-3)*-1 # reverse (1:2) to (2:1)
 
-reverse_P8d <- P8d[2]
-df[reverse_P8d] <- (df[reverse_P8d]-3)*-1 # reverse (1:2) to (2:1)
+df[P10c[4]] <- ifelse(df[P10c[4]] == 1, 1, 0) # 1 is correct
 
-df[P8d[4]] <- ifelse(df[P8d[4]] == 1, 1, 0) # 1 is correct
+P9b1 <- P9b[1:2]
+P9b2 <- P9b[3]
+P9b3 <- P9b[4:5]
 
-P8ba <- P8b[1:2]
-P8bb <- P8b[3]
-P8bc <- P8b[4:5]
-P8da <- P8d[1:2]
-P8db <- P8d[3]
-P8dc <- P8d[4]
+P10a <- P10a_b[1]
+P10b <- P10a_b[2]
 
-P8c1 <- P8c[1]
-P8c2 <- P8c[2]
+P10c1 <- P10c[1:2]
+P10c2 <- P10c[3]
+P10c3 <- P10c[4]
 
 df <- df %>% 
   mutate(q1a = rowSums(.[P1a], na.rm = FALSE), # True/False questions
          q1b = rowSums(.[P1b], na.rm = FALSE), # MCQ questions
          q2 = rowSums(.[P2], na.rm = FALSE), # Subjective Happiness (SHS) range(1-7)
-         q3 = rowSums(.[P3], na.rm = FALSE),# Anxiety (SCARED) range(0-18)
-         q4neg = rowSums(.[reverse_P4], na.rm = FALSE),  # Negative Thinking (CATS-N/P) range(0-40)
+         q3 = rowSums(.[P3], na.rm = FALSE), # Anxiety (SCARED) range(0-18)
+         q4neg = rowSums(.[reverse_P4], na.rm = FALSE), # Negative Thinking (CATS-N/P) range(0-40)
          q4pos = rowSums(.[P4[!(P4 %in% reverse_P4)]], na.rm = FALSE),  # Positive Thinking (CATS-N/P) range(0-40)
          q5 = rowSums(.[P5], na.rm = FALSE), # C-IRI perspective taking range(0-24)
          q6 = rowSums(.[P6], na.rm = FALSE), # Self-esteem (RSES) range(10-40)
          q7 = rowSums(.[P7], na.rm = FALSE), # Gratitude GQ-6 range(6-42)
-         
-         q8a = rowMeans(.[P8a], na.rm = FALSE), # Compassion - common humanity subscale range(0-4), mean instead of sum
-         q8b = rowMeans(.[P8b], na.rm = FALSE), # self-compassion range(0-4), mean instead of sum
-         q8ba = rowMeans(.[P8ba], na.rm = FALSE), # self-compassion - self-kindness subscale range(0-4), mean instead of sum
-         q8bb = .[[P8bb]], # self-compassion - self-judgement subscale range(0-4), mean instead of sum
-         q8bc = rowMeans(.[P8bc], na.rm = FALSE), # self-compassion - common humanity subscale range(0-4), mean instead of sum
-         # q8c = rowSums(.[P8c], na.rm = FALSE), # help-seeking, range(2-5)
-         q8da =  rowMeans(.[P8da], na.rm = FALSE), # prejudice - fear/avoidance subscale range(0-2), mean instead of sum
-         q8db =  .[[P8db]], # prejudice - unpredictability subscale range(0-2), mean instead of sum
-         q8d2 =  .[[P8dc]] # understanding subscale range(0-1), mean instead of sum
+         q9a = rowMeans(.[P9a], na.rm = FALSE), # Compassion - common humanity subscale range(0-4), mean instead of sum
+         q9b = rowMeans(.[P9b], na.rm = FALSE), # self-compassion range(0-4), mean instead of sum
+         q9b1 = rowMeans(.[P9b1], na.rm = FALSE), # self-compassion - self-kindness subscale range(0-4), mean instead of sum
+         q9b2 = .[[P9b2]], # self-compassion - self-judgement subscale range(0-4), mean instead of sum
+         q9b3 = rowMeans(.[P9b3], na.rm = FALSE), # self-compassion - common humanity subscale range(0-4), mean instead of sum
+         q10a_b = rowSums(.[P10a_b], na.rm = FALSE), # help-seeking, range(2-5)
+         q10c1 =  rowMeans(.[P10c1], na.rm = FALSE), # prejudice - fear/avoidance subscale range(0-2), mean instead of sum
+         q10c2 =  .[[P10c2]], # prejudice - unpredictability subscale range(0-2), mean instead of sum
+         q10c3 =  .[[P10c3]], # understanding subscale range(0-1), mean instead of sum
+         q10c = rowMeans(.[P10c], na.rm = FALSE) # partial PPMI range(1-2), mean instead of sum
   )
 
-df$q1 <- df$q1a + df$q1b # Mental Health Knowledge range(0-15)
-df$q8d1 <- (df$q8da*2 + df$q8db)/3 # weighted average because q8da has two questions
+df$q1 <- df$q1a + df$q1b # Mental Health Knowledge range (0-21)
 
-df$q8c1 <- as.factor(df[[P8c1]])
-levels(df$q8c1) <- c("Yes","No","Not sure")
+df$q10c1_2 <- (df$q10c1*2 + df$q10c2)/3 # weighted average because q8da has two questions
 
-df$q8c2 <- as.factor(df[[P8c2]])
-levels(df$q8c2) <- c("Yes","No")
+df$q10a <- as.factor(df[[P10a]])
+levels(df$q10a) <- c("Yes","No","Not sure")
 
-df$q8d2 <- as.factor(df$q8d2)
-levels(df$q8d2) <- c("Incorrect","Correct")
+df$q10b <- as.factor(df[[P10b]])
+levels(df$q10b) <- c("Yes","No")
 
-# selective program for 2020-2021 (S2) ----
-df_se <- df %>% # selective
-  filter(!is.na(sch)) %>%
-  filter(T1 == 0 & sch == 2 & grade == 4) 
+df$q10c3 <- as.factor(df$q10c3)
+levels(df$q10c3) <- c("Incorrect","Correct")
 
-df_mean_T0 <- df %>%
-  filter(!is.na(sch)) %>%
-  filter(T1 == 0 & sch == 2) %>%
-  group_by(sch) %>%
-  summarise_at(c("q1", "q2", "q3", "q4neg", "q4pos", "q5", "q6", "q7"), mean, na.rm = TRUE)
+dflevel1 <- df
+rm(df)
 
-# df_se <- distinct(df_se, sch, class, student_num, dob, .keep_all = TRUE) # keep only first instance of same student & T1 (i.e. remove any repeats)
+# # selective program for 2020-2021 (S2) ----
+# df_se <- df %>% # selective
+#   filter(!is.na(sch)) %>%
+#   filter(T1 == 0 & sch == 2 & grade == 4) 
+# 
+# df_mean_T0 <- df %>%
+#   filter(!is.na(sch)) %>%
+#   filter(T1 == 0 & sch == 2) %>%
+#   group_by(sch) %>%
+#   summarise_at(c("q1", "q2", "q3", "q4neg", "q4pos", "q5", "q6", "q7"), mean, na.rm = TRUE)
+# 
+# # df_se <- distinct(df_se, sch, class, student_num, dob, .keep_all = TRUE) # keep only first instance of same student & T1 (i.e. remove any repeats)
+# 
+# df_se$sex <- ifelse(df_se$sex==2, "M", "F")
+# df_se <- df_se %>% arrange(df_se$sch, df_se$class, df_se$student_num)
+# 
+# table_loop <- function(df_se, df_mean){
+#   wb <- xlsx::createWorkbook(type="xlsx")
+#   i <- 2
+#   for (i in (1:17)){
+#     if (i %in% df_se$sch){
+#       df_selective <- df_se %>% # selective
+#         filter(sch ==  i) %>%
+#         mutate(q6_rank = order(order(q6))) # rank q6 values by school from lowest to highest
+#       
+#       if (i==11){
+#         selective_n <- 48 # 25 more students  are wanted on top of the original 23
+#       } else {
+#         selective_n <- 20 # select at least 20 students
+#       }
+#       df_selective <- df_selective %>% filter((q6 <= max(.$q6[.$q6_rank<=selective_n])))
+#       
+#       df_selective_mean <- df_selective %>%
+#         filter(!is.na(sch)) %>%
+#         filter(T1 == 0) %>%
+#         group_by(sch) %>%
+#         summarise_at(c("q1", "q2", "q3", "q4neg", "q4pos", "q5", "q6", "q7"), mean, na.rm = TRUE)
+#       
+#       table <- data.frame(matrix(ncol = 13,  nrow = 0))
+#       table <- subset(df_selective[df_selective$sch==i,], select = c("class",
+#                                                                      "student_num",
+#                                                                      "sex",
+#                                                                      "dob",
+#                                                                      "q1", "q2", "q3", "q4neg", "q4pos", "q5", "q6", "q7", "imputed_sch"))
+#       colnames(table)  <-  c("Class",
+#                              "Student Number",
+#                              "Sex",
+#                              "Date of Birth",
+#                              "Mental Health Knowledge",
+#                              "Subjective Happiness"	,
+#                              "Anxiety",
+#                              "Negative Thinking",
+#                              "Positive Thinking",
+#                              "Perspective Taking (Empathy)",
+#                              "Self-esteem",
+#                              "Gratitude",
+#                              "Matched from IP address")
+#       
+#       table$`Date of Birth` <- ifelse(is.na(table$`Date of Birth`), NA,
+#                                       as.character.Date(table$`Date of Birth`, tryFormats = "%Y/%m/%d"))
+#       table[nrow(table)+1,4] <- "Mean of Group with Lowest Self-esteem"
+#       table[nrow(table)+1,4] <- "School Mean (Pre-test)"
+#       table[nrow(table)-1,5:(length(table)-1)] <- sapply(round(df_selective_mean[df_selective_mean$sch==i,2:length(df_selective_mean)], 2),function(x)format(x,nsmall = 2))
+#       table[nrow(table),5:(length(table)-1)] <- sapply(round(df_mean[df_mean$sch==i,2:length(df_mean)], 2),function(x)format(x,nsmall = 2))
+#       
+#       sheet <- xlsx::createSheet(wb, sheetName = paste0("S", deparse(as.numeric(i))))
+#       assign(paste0("sheet", deparse(as.numeric(i))), sheet)
+#       xlsx::addDataFrame(as.data.frame(table), get_("sheet", as.numeric(i)),
+#                          startRow=1, startColumn=1,
+#                          row.names = FALSE, showNA = FALSE)
+#     }
+#   }
+#   xlsx::saveWorkbook(wb, "qtn1920_pri_selective_qtn2021.xlsx", password=NULL)
+# }
+# 
+# setwd(sprintf("~%s/qtn/qtn2019-20/primary/qtn1920_primary_selective_qtn2021", setpath))
+# 
+# table_loop(df_se, df_mean_T0)
 
-df_se$sex <- ifelse(df_se$sex==2, "M", "F")
-df_se <- df_se %>% arrange(df_se$sch, df_se$class, df_se$student_num)
+# P5 (level 2) ----
+dfpre <- haven::read_sav("P5-pre-survey_450002_SPSS.sav")
+dfpost <- haven::read_sav("P5-post-survey_450005_SPSS.sav")
 
-table_loop <- function(df_se, df_mean){
-  wb <- xlsx::createWorkbook(type="xlsx")
-  i <- 2
-  for (i in (1:17)){
-    if (i %in% df_se$sch){
-      df_selective <- df_se %>% # selective
-        filter(sch ==  i) %>%
-        mutate(q6_rank = order(order(q6))) # rank q6 values by school from lowest to highest
-      
-      if (i==11){
-        selective_n <- 48 # 25 more students  are wanted on top of the original 23
-      } else {
-        selective_n <- 20 # select at least 20 students
-      }
-      df_selective <- df_selective %>% filter((q6 <= max(.$q6[.$q6_rank<=selective_n])))
-      
-      df_selective_mean <- df_selective %>%
-        filter(!is.na(sch)) %>%
-        filter(T1 == 0) %>%
-        group_by(sch) %>%
-        summarise_at(c("q1", "q2", "q3", "q4neg", "q4pos", "q5", "q6", "q7"), mean, na.rm = TRUE)
-      
-      table <- data.frame(matrix(ncol = 13,  nrow = 0))
-      table <- subset(df_selective[df_selective$sch==i,], select = c("class",
-                                                                     "student_num",
-                                                                     "sex",
-                                                                     "dob",
-                                                                     "q1", "q2", "q3", "q4neg", "q4pos", "q5", "q6", "q7", "imputed_sch"))
-      colnames(table)  <-  c("Class",
-                             "Student Number",
-                             "Sex",
-                             "Date of Birth",
-                             "Mental Health Knowledge",
-                             "Subjective Happiness"	,
-                             "Anxiety",
-                             "Negative Thinking",
-                             "Positive Thinking",
-                             "Perspective Taking (Empathy)",
-                             "Self-esteem",
-                             "Gratitude",
-                             "Matched from IP address")
-      
-      table$`Date of Birth` <- ifelse(is.na(table$`Date of Birth`), NA,
-                                      as.character.Date(table$`Date of Birth`, tryFormats = "%Y/%m/%d"))
-      table[nrow(table)+1,4] <- "Mean of Group with Lowest Self-esteem"
-      table[nrow(table)+1,4] <- "School Mean (Pre-test)"
-      table[nrow(table)-1,5:(length(table)-1)] <- sapply(round(df_selective_mean[df_selective_mean$sch==i,2:length(df_selective_mean)], 2),function(x)format(x,nsmall = 2))
-      table[nrow(table),5:(length(table)-1)] <- sapply(round(df_mean[df_mean$sch==i,2:length(df_mean)], 2),function(x)format(x,nsmall = 2))
-      
-      sheet <- xlsx::createSheet(wb, sheetName = paste0("S", deparse(as.numeric(i))))
-      assign(paste0("sheet", deparse(as.numeric(i))), sheet)
-      xlsx::addDataFrame(as.data.frame(table), get_("sheet", as.numeric(i)),
-                         startRow=1, startColumn=1,
-                         row.names = FALSE, showNA = FALSE)
-    }
-  }
-  xlsx::saveWorkbook(wb, "qtn1920_pri_selective_qtn2021.xlsx", password=NULL)
-}
+dfpre$T1 <- 0
+dfpost$T1 <- 1
+df <- plyr::rbind.fill(dfpre, dfpost)
+rm(dfpre, dfpost)
 
-setwd(sprintf("~%s/qtn/qtn2019-20/primary/qtn1920_primary_selective_qtn2021", setpath))
-
-table_loop(df_se, df_mean_T0)
+names(df)[names(df)=="Q01"] <- "class" 
+df$grade <- as.integer(as.vector(substr(df$class,1,1)))
+names(df)[names(df)=="Q02"] <- "student_num"
+names(df)[names(df)=="Q03"] <- "dob"
+df$age <- as.numeric(floor((df$submitdate-df$dob)/365.2425))
