8956195f998c8e31876b7cb0a26186d27fa2c607
King <king.wa.tam@gmail.com>
2020-12-03 15:33:50 +0800

    Re-organise structure and update descriptions of functions

--- helper_functions.R
+++ helper_functions.R
@@ -8,21 +8,26 @@
 ##
 ## Notes:
 ## summ() & tab() work like sum & tab functions in Stata
-## round_format() 
-## starred_p()
-## eval_() simplifies eval(parse()) globally, while concatenating strings so that paste/sprintf functions are not needed
-## get_() works like get() in the global environment
-## trycatchNA() when a function results in an error, returns NA
+## summ() e.g. summ(df$var1, df$var2, ...) or summ(df1, df2, ....) or sum(df$var1, df2)
+## tab() e.g. tab(df$var1, df$var2, df$var3….)
+## round_format(value, decimal_places =  2, check_object = FALSE) 
+## starred_p(p_value, decimal_places, related_value = NULL)
+## recode_age(var, age_labels = NULL, second_group = 25, interval = 5, last_group = 65) recodes age into age groups - either arbitrary age groups by providing age_lables or by a fixed interval. 
+#### Default values are equivalent to using age_labels = c("0-24", "25-29" ,"30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65+") 
+#### Otherwise, skip the age_labels option by specifying the second group starting age, and fixed interval and the last group starting age, i.e. recode_age(df$age, , 25, 5, 65). 
+## import_func() imports only functions from another R script, analagous to the base source() function but ignores non-functions inside the script
 ## write_excel() writes tables as multiple sheets in an Excel file
-## convert2NA() replaces certain values with NAs
-## convert2value() replaces certain values with any single value
-## after_dollarsign() gets string after dollar sign
-## get_freqtable() returns up to 3-way frequency table as a dataframe
 ## iferror() works like the iferror function in Excel
 ## ifwarning() works like iferror for warnings instead
-## recode_age() recodes age into age groups - either arbitrary age groups by providing age_lables or age_groups by a fixed interval
+## trycatchNA() when a function results in an error, returns NA. trycatch_() is the same except returns a user-defined object
+## convert2NA() replaces user-defined values with NAs
+## convert2value() replaces certain values with any single value
+## after_dollarsign() returns string after dollar sign
+## after_char() and before_char() return string after/before a given character
+## get_freqtable() returns up to 3-way frequency table as a data frame
+## eval_() simplifies eval(parse()) globally, while concatenating strings so that paste/sprintf functions are not needed. Beware of certain problems since it evaluates objects from the global environment
+## get_() works like get() in the global environment. Beware of certain problems since it gets objects from the global environment
 ## summary.lm() lm summary for robust (sandwich) SEs and clustered SEs (up to 2 cluster variables)
-## import_func() imports only functions from an R script
 
 sum_func <- function(x, var_name) { # sum_func() is used inside summ()
   argx <- var_name
@@ -149,36 +154,34 @@ starred_p <- function(p_value, decimal_places, related_value = NULL){
   }
 }
 
-eval_<- function(...){ # evaluate text & variables as expression 
-  n_args <- nargs()
-  arg_values <- list(...)
-  myvector <- c()
-  if (n_args>0) {
-    for (i in 1:n_args) {
-      myvector[i] <- arg_values[[i]]
-    }
+recode_age <- function(var, age_labels = NULL, second_group = 25, interval = 5, last_group = 65){ # either input age_labels directly or input starting value for second age group, the interval, and starting value for the last group
+  if (is.null(age_labels)){
+    starting_group <- paste0('0-', second_group - 1)
+    
+    age_labels <- c(starting_group, paste(seq(second_group, last_group-interval, by = interval), seq(second_group + interval - 1, last_group - 1, by = interval),
+                                          sep = "-"), paste(last_group, "+", sep = ""))
+    
+    return(
+      cut(as.integer(var), breaks = c(0, seq(second_group, last_group, by = interval), Inf), labels = age_labels, right = FALSE)
+    )
+  } else {
+    return(
+      cut(as.integer(var), breaks = c(as.vector(as.numeric(before_char(age_labels, "-|+"))), Inf), labels = age_labels, right = FALSE)
+    )
   }
-  return(eval(parse(text = paste0(myvector, collapse = "")), parent.frame())) # evaluating in parent environment
 }
 
-get_<- function(...){ # evaluate text as expression (faster than eval(parse()) by a factor of 2-7!)
-  n_args <- nargs()
-  arg_values <- list(...)
-  myvector <- c()
-  if (n_args>0) {
-    for (i in 1:n_args) {
-      myvector[i] <- arg_values[[i]]
+import_func <- function(R_file){
+  expressions <- parse(R_file)
+  functions_only <- sapply(expressions, function(x) {
+    if(x[[1]]=="<-") {
+      if(x[[3]][[1]]=="function") {
+        return(TRUE)
+      }
     }
-  }
-  return(get(paste0(myvector, collapse = ""), parent.frame() )) # evaluating in parent.frame()
-}
-
-trycatchNA <- function(func){
-  tryCatch(func, error=function(err) NA)
-}
-
-trycatch_ <- function(func, x){
-  tryCatch(func, error=function(err) x)
+    return(FALSE)
+  })
+  eval(expressions[functions_only], parent.frame())
 }
 
 write_excel <- function(filename = "sheet.xlsx", ..., remove_char =  NULL){ 
@@ -207,10 +210,29 @@ write_excel <- function(filename = "sheet.xlsx", ..., remove_char =  NULL){
     }
     sheet <- xlsx::createSheet(wb, sheetName = sheetname)  # certain punctuation marks are removed due to Excel worksheet names not allowing them
     xlsx::addDataFrame(as.data.frame(arg_values[[i]]), sheet,
-                 startRow=1, startColumn=1,
-                 row.names = FALSE, showNA = FALSE)
+                       startRow=1, startColumn=1,
+                       row.names = FALSE, showNA = FALSE)
   }
   xlsx::saveWorkbook(wb, filename, password=NULL)
+}
+
+iferror <- function(expr, error_expr){
+  tryCatch(expr, 
+           error=function(e){error_expr}
+  )
+}
+
+ifwarning <- function(expr, warning_expr){
+  tryCatch(expr, 
+           warning=function(w){warning_expr})
+}
+
+trycatchNA <- function(func){
+  tryCatch(func, error=function(err) NA)
+}
+
+trycatch_ <- function(func, x){
+  tryCatch(func, error=function(err) x)
 }
 
 convert2NA <- function(df, values){
@@ -230,17 +252,17 @@ convert2NA <- function(df, values){
 }
 
 convert2value <- function(df, values, value){
-    for (c in (1:ncol(df))){
-      was_factor <- FALSE
-      if (is.factor(df[,c])){ # change from factor to character
-        was_factor <- TRUE
-        df[,c] <- as.character(df[,c])
-      }
-      df[df[,c] %in% values, c] <- value
-      if (was_factor){ 
-        df[,c] <- as.factor(df[,c])
-      }
+  for (c in (1:ncol(df))){
+    was_factor <- FALSE
+    if (is.factor(df[,c])){ # change from factor to character
+      was_factor <- TRUE
+      df[,c] <- as.character(df[,c])
     }
+    df[df[,c] %in% values, c] <- value
+    if (was_factor){ 
+      df[,c] <- as.factor(df[,c])
+    }
+  }
   return(df)
 }
 
@@ -284,46 +306,34 @@ get_freqtable <- function(x, y=NULL, z=NULL){
   return(dat)
 }
 
-iferror <- function(expr, error_expr){
-  tryCatch(expr, 
-           error=function(e){error_expr}
-           )
-}
-
-ifwarning <- function(expr, warning_expr){
-  tryCatch(expr, 
-           warning=function(w){warning_expr})
-}
-
-recode_age <- function(var, age_labels = NULL, second_group = 25, interval = 5, last_group = 65){ # either input age_labels directly or input starting value for second age group, the interval, and starting value for the last group
-  if (is.null(age_labels)){
-    starting_group <- paste0('0-', second_group - 1)
-    
-    age_labels <- c(starting_group, paste(seq(second_group, last_group-interval, by = interval), seq(second_group + interval - 1, last_group - 1, by = interval),
-                                          sep = "-"), paste(last_group, "+", sep = ""))
-
-    return(
-      cut(as.integer(var), breaks = c(0, seq(second_group, last_group, by = interval), Inf), labels = age_labels, right = FALSE)
-    )
-  } else {
-    return(
-      cut(as.integer(var), breaks = c(as.vector(as.numeric(before_char(age_labels, "-|+"))), Inf), labels = age_labels, right = FALSE)
-    )
-  }
-}
-
-import_func <- function(R_file){
-  expressions <- parse(R_file)
-  functions_only <- sapply(expressions, function(x) {
-    if(x[[1]]=="<-") {
-      if(x[[3]][[1]]=="function") {
-        return(TRUE)
-      }
+eval_<- function(...){ # evaluate text & variables as expression 
+  n_args <- nargs()
+  arg_values <- list(...)
+  myvector <- c()
+  if (n_args>0) {
+    for (i in 1:n_args) {
+      myvector[i] <- arg_values[[i]]
     }
-    return(FALSE)
-  })
-  eval(expressions[functions_only], parent.frame())
+  }
+  return(eval(parse(text = paste0(myvector, collapse = "")), parent.frame())) # evaluating in parent environment
 }
+
+get_<- function(...){ # evaluate text as expression (faster than eval(parse()) by a factor of 2-7!)
+  n_args <- nargs()
+  arg_values <- list(...)
+  myvector <- c()
+  if (n_args>0) {
+    for (i in 1:n_args) {
+      myvector[i] <- arg_values[[i]]
+    }
+  }
+  return(get(paste0(myvector, collapse = ""), parent.frame() )) # evaluating in parent.frame()
+}
+
+
+
+
+
 
 # robust SEs for lm()
 # import the function from repository
