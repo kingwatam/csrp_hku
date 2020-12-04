29083bf84127218a6a3557b441f2acd03102f06a
King <king.wa.tam@gmail.com>
2020-12-04 13:10:03 +0800

    Generate ngram with and without stop words

--- poverty/monograph/book_indexing.R
+++ poverty/monograph/book_indexing.R
@@ -8,6 +8,7 @@ library(dplyr)
 library(textreadr) # read_docx
 library(quanteda) # tokens, tokens_remove
 library(tidytext) # stop_words dataset
+library(stringr)
 # library(stopwords)
 
 setwd(sprintf("~%s/poverty/monograph", setpath))
@@ -16,23 +17,38 @@ setwd(sprintf("~%s/poverty/monograph", setpath))
 # saveRDS(df, 'monograph_25.11-pyip.RDS')
 
 df <- readRDS("monograph_25.11-pyip.RDS")
+
 words <- quanteda::tokens(df,
-                what="word",
-                remove_numbers=TRUE,
-                remove_punct=TRUE,
-                remove_symbols=TRUE,
-                remove_separators=TRUE,
-                remove_url=TRUE) 
+                           what="word",
+                           remove_numbers=TRUE,
+                           remove_punct=TRUE,
+                           remove_symbols=TRUE,
+                           remove_separators=TRUE,
+                           remove_url=TRUE) 
 
-data("stop_words")
-
-new_stop_words <- c("Hong", "Kong", "Kong's", "HK",
-                    "fig", "figures", "figure", "tables", "table",
-                    "Yip", "al", "", " ", "final.pdf" , "e.g.", "i.e.")
-
-words  <- tokens_remove(words, c(stop_words$word, new_stop_words)) # remove stop words
-
-get_ngram <- function(tokens, n){
+get_ngram <- function(tokens, n, remove_stop_words = TRUE, capitalized_only = FALSE){
+  if (capitalized_only){
+    df <- tokens # input must be df instead of tokens
+    df <- stringr::str_extract_all(df, "[A-Z][a-z]+[ ]*")
+    tokens <- quanteda::tokens(df,
+                              what="word",
+                              remove_numbers=TRUE,
+                              remove_punct=TRUE,
+                              remove_symbols=TRUE,
+                              remove_separators=TRUE,
+                              remove_url=TRUE) 
+  }
+  
+  new_stop_words <- c("Hong", "Kong", "Kong's", "HK", "Hong Kong",
+                      "fig", "figures", "figure", "tables", "table",
+                      "Yip", "al", "", " ", "final.pdf" , "e.g.", "i.e.")
+  if (remove_stop_words){
+    data("stop_words")
+    tokens  <- tokens_remove(tokens, c(stop_words$word, new_stop_words)) # remove stop words
+  } else {
+    tokens  <- tokens_remove(tokens, c(new_stop_words)) # remove stop words
+  }
+  
   words <- tokens_ngrams(tokens, n = n)
   words <- paste(words)
   words <- tokens(words,
@@ -58,7 +74,13 @@ get_ngram <- function(tokens, n){
 ngram1 <- get_ngram(words, n = 1)
 ngram2 <- get_ngram(words, n = 2)
 ngram3 <- get_ngram(words, n = 3)
+ngram3_ <- get_ngram(words, n = 3, remove_stop_words = FALSE)
+# ngram4 <- get_ngram(words, n = 4)
+# ngram4_ <- get_ngram(words, n = 4, remove_stop_words = FALSE)
+# ngram5 <- get_ngram(words, n = 5)
+# ngram5_ <- get_ngram(words, n = 5, remove_stop_words = FALSE)
 
+ngram1cap <- get_ngram(df, n = 1, capitalized_only = TRUE) # input must be df instead of tokens
 
-write_excel("monograph_ngram.xlsx", ngram1, ngram2, ngram3)
-
+write_excel("monograph_ngram.xlsx", 
+            ngram1, ngram1cap, ngram2, ngram3, ngram3_)
